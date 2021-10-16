f90e807483234a3625594e9936fd719bbe94de42
tongxh21 <tongxh@mails.tsinghua.edu.cn>
2021-10-16 23:35:42 +0800

    third

---
+++ problem_2.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[79]:
+
+
+a=input()
+
+
+# In[80]:
+
+
+a=a.split()
+a=list(a)
+print(a[1])
+print(a[0])
+print(a[2])
+
+
+# In[81]:
+
+
+if float(a[0])+float(a[1])>float(a[2]) and float(a[1])+float(a[2])>float(a[0]) and float(a[2])+float(a[0])>float(a[1]):
+    print("可以构成合法三角形")
+else:
+    print("不能构成合法三角形")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
